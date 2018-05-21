9bc58841a590ffef91fc42e58bdfcfad193c2c99
KOITT <KOITT@KOITT-PC>
2018-05-21 21:31:05 +0900

    세번째 수정...

--- ch01/src/hello.java
+++ ch01/src/hello.java
@@ -4,6 +4,8 @@ public class hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello");
+		System.out.println("안녕 세계");
+		System.out.println("home");
 	}
 
 }
